c34e511641dfe1594ce766055a0ae678741776ad
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-05-16 12:39:03 +0530

    add food image

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import mealsImage from './../../assets/meals.jpg'
+import classes from './header.module.css'
+
+function Header() {
+  return (
+    <React.Fragment>
+        <header className={classes.header}>
+            <h1>React Meals</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt='image of foods'/>
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Header
