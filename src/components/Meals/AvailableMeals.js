1394df0fe5b4632b20d0026e754b6eeb3f646c32
Janith Lahiru Kariyawasam <88109775+janithlahirukariyawasam@users.noreply.github.com>
2023-05-18 18:27:43 +0530

    add Meals

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AvailableMeals() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AvailableMeals
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MealsSummary() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default MealsSummary
